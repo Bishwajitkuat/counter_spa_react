f2a74a362a3dfc89cbe19f895e479908d48411df
Bishwajitkuat <bishwajitkuat@gmail.com>
2023-04-01 11:20:04 +0300

    backgroud color change depending on the number zero or odd or even

--- src/Main.js
+++ src/Main.js
@@ -13,12 +13,15 @@ class Main extends Component {
       } else this.setState({ number: this.state.number + nextOperation });
     }
   };
+
   render() {
+    let cirClass = "circle";
+    if (this.state.number === 0) cirClass = "circle";
+    else if (this.state.number % 2 === 0) cirClass = "circle even";
+    else cirClass = "circle odd";
     return (
       <div className="mainDiv">
-        <div className="countDiv">
-          <p className="countP">{this.state.number}</p>
-        </div>
+        <p className={cirClass}>{this.state.number}</p>
         <div className="buttonDiv">
           <Button id="5" text="Add 5" hendelar={(e) => this.btnClick(e)} />
           <Button id="1" text="Add 1" hendelar={(e) => this.btnClick(e)} />
